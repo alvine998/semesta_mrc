4abc84d57f00b57bd7e0fbfebf8533fd3cf25d57
alvin <alvinecom2018@gmail.com>
2021-09-07 15:42:13 +0700

    home full

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,10 +2,25 @@ import logo from './images/splash_logo.png';
 import banner1 from './images/banner1.png';
 import banner2 from './images/banner2.png';
 import banner3 from './images/banner3.png';
+import jakarta from './icons/jakarta.png';
+import bandung from './icons/bandung.png';
+import jogja from './icons/jogja.png';
+import bogor from './icons/bogor.png';
+import depok from './icons/depok.png';
+import surabaya from './icons/surabaya.png';
+import bannerpromo from './images/bannerpromo.jpg';
+
 
 export {
     logo,
     banner1,
     banner2,
-    banner3
+    banner3,
+    jakarta,
+    surabaya,
+    bandung,
+    jogja,
+    bogor,
+    depok,
+    bannerpromo
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
 import normalize from 'react-native-normalize';
-import { logo } from '../assets';
+import { bandung, bannerpromo, bogor, depok, jakarta, jogja, logo, surabaya } from '../assets';
 import {SliderBox} from 'react-native-image-slider-box';
 
 export default class Home extends Component{
@@ -30,39 +30,69 @@ export default class Home extends Component{
                     <View style={{paddingTop:normalize(50)}}>
                         <SliderBox autoplay circleLoop images={this.state.images}/>
                     </View>
-
+                    {/* Area Terapi MRC */}
                     <View style={styles.position2}>
                         <View style={styles.theme2}>
-                            <View >
+                            <View>
                                 <Text style={styles.text1}>MAU TERAPI DIMANA ?</Text> 
                             </View>
                             <View style={styles.position3}>
-                                <View style={styles.square1}>
-
-                                </View>
+                                <TouchableOpacity>
+                                    <View style={styles.square1}>
+                                        <Image source={jakarta} style={{width:normalize(60), height:normalize(80)}} />   
+                                    </View>
+                                    <Text style={styles.text2}>JAKARTA</Text> 
+                                </TouchableOpacity>
                                 <View style={{paddingRight:normalize(20)}} />
-                                <View style={styles.square1}>
-
-                                </View>
+                                <TouchableOpacity>
+                                    <View style={styles.square1}>
+                                        <Image source={bogor} style={{width:normalize(60), height:normalize(80)}} />   
+                                    </View>
+                                    <Text style={styles.text2}>BOGOR</Text> 
+                                </TouchableOpacity>
                                 <View style={{paddingRight:normalize(20)}} />
-                                <View style={styles.square1}>
-
-                                </View>
+                                <TouchableOpacity>
+                                    <View style={styles.square1}>
+                                        <Image source={depok} style={{width:normalize(60), height:normalize(80)}} />   
+                                    </View>
+                                    <Text style={styles.text2}>DEPOK</Text> 
+                                </TouchableOpacity>
                             </View>
-                            <View style={styles.position3}>
-                                <View style={styles.square1}>
-
-                                </View>
+                            <View style={styles.position4}>
+                                <TouchableOpacity onPress={() => Alert.alert("Belum Tersedia Di Bandung")}>
+                                    <View style={styles.square1}>
+                                        <Image source={bandung} style={{width:normalize(60), height:normalize(80)}} />   
+                                    </View>
+                                    <Text style={styles.text2}>BANDUNG</Text> 
+                                </TouchableOpacity>
                                 <View style={{paddingRight:normalize(20)}} />
-                                <View style={styles.square1}>
-
-                                </View>
+                                <TouchableOpacity onPress={() => Alert.alert("Belum Tersedia Di Jogja")}>
+                                    <View style={styles.square1}>
+                                        <Image source={jogja} style={{width:normalize(60), height:normalize(80)}} />   
+                                    </View>
+                                    <Text style={styles.text2}>JOGJA</Text> 
+                                </TouchableOpacity>
                                 <View style={{paddingRight:normalize(20)}} />
-                                <View style={styles.square1}>
-
-                                </View>
+                                <TouchableOpacity onPress={() => Alert.alert("Belum Tersedia Di Surabaya")}>
+                                    <View style={styles.square1}>
+                                        <Image source={surabaya} style={{width:normalize(60), height:normalize(80)}} />   
+                                    </View>
+                                    <Text style={styles.text2}>SURABAYA</Text> 
+                                </TouchableOpacity>
                             </View>
                         </View>
+
+                        {/* Spesial Promo */}
+                        <View style={{paddingTop:normalize(40)}}>
+                            <Text style={styles.text3}>SPESIAL PROMO</Text>
+                            <View style={{alignItems:'center', justifyContent:'center'}}>
+                                <Image source={bannerpromo} style={{width:normalize(300), height:normalize(185)}} />
+                            </View>
+                        </View>
+                    </View>
+                    {/* copyright */}
+                    <View>
+                        <Text style={{textAlign:'center', color:'white', fontSize:normalize(12)}}>Copyright Semesta Group Bertasbih 2021</Text>
                     </View>
                 </ScrollView>
             </View>
@@ -85,20 +115,27 @@ const styles = StyleSheet.create({
     theme2:{
         backgroundColor:'#93108D',
         borderRadius:20,
-        height:normalize(280),
+        height:normalize(300),
         width:'100%',
-        padding:normalize(20)
+        padding:normalize(20),
     },
     position2:{
         paddingTop:normalize(50),
         paddingLeft:normalize(20),
-        paddingRight:normalize(20)
+        paddingRight:normalize(20),
+        paddingBottom:normalize(20)
     },
     position3:{
         flexDirection:'row',
         padding:normalize(20),
         alignItems:'center',
         justifyContent:'center'
+    },
+    position4:{
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center',
+        marginLeft:normalize(15)
     },
     text1:{
         fontFamily:'RedHatDisplay-Regular',
@@ -109,6 +146,20 @@ const styles = StyleSheet.create({
     square1:{
         height:normalize(70),
         width:normalize(70),
-        backgroundColor:'#A746A3'
+        backgroundColor:'#A746A3',
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    text2:{
+        fontFamily:'RedHatDisplay-Regular',
+        fontSize:normalize(18),
+        color:'white',
+        textAlign:'center'
+    },
+    text3:{
+        fontFamily:'RedHatDisplay-Bold',
+        fontSize:normalize(24),
+        color:'white',
+        textAlign:'center'
     }
 })
